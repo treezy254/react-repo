29a65dfe68b891feae20b71705a033f263baa8b4
nccdevops <146719391+nccdevops@users.noreply.github.com>
2023-10-14 12:43:35 +0000

    Add files via upload

---
+++ src/reducers/profile.js
@@ -0,0 +1,19 @@
+import { SET_USER_ID } from "../actions/types";
+
+/**
+ * @export
+ * @param {any} [state=[]]
+ * @param {any} [action={}]
+ *@returns {Array} state
+*/
+
+export default function user(state = [], action = {}) {
+	switch(action.type) {
+		case SET_USER_ID:
+			return [
+				...state,
+				action.id,
+			];
+		default: return state;
+	}
+}
