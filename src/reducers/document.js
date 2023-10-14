1f1be1c75be47ff60830b8160a16910cb4ce6531
nccdevops <146719391+nccdevops@users.noreply.github.com>
2023-10-14 12:22:03 +0000

    Add files via upload

---
+++ src/reducers/document.js
@@ -0,0 +1,49 @@
+import { SET_DOCUMENTS, ADD_DOCUMENT, DOCUMENT_FETCHED, DOCUMENT_UPDATED, DOCUMENT_DELETED } from "../actions/types";
+
+/**
+ * @export
+ * @param {any} [state=[]]
+ * @param {any} [action={}]
+ * @returns {Array} state
+ */
+
+export default function document(state = [], action = {}) {
+	switch (action.type) {
+		
+		case ADD_DOCUMENT:
+			return [
+				...state,
+				action.document,
+			];
+
+		case DOCUMENT_FETCHED:
+			{
+				const index = state.findIndex(item => item.id === action.document.id);
+				if (index > -1) {
+					return state.map(item => {
+						if (item.id === action.document.id) return action.document;
+						return item;
+					});
+				} else {
+					return [
+						...state,
+						action.document,
+					];
+				}
+			}
+
+		case DOCUMENT_UPDATED:
+			return state.amp(item => {
+				if (item.id === action.document.id) return action.document;
+				return item;
+			});
+
+		case DOCUMENT_DELETED:
+			return state.filter(item => item.id !== action.documentId);
+
+		case SET_DOCUMENTS:
+			return action.documents;
+
+		default: return state;
+	}
+}
